9bdc5d68d4597d6a7e894d4cf291812e0dfc86b3
Rodrigo Baggio <rodrigo.baggio.si@gmail.com>
2022-01-13 17:09:45 -0300

    DELETE Category

--- backend/src/main/java/com/devsuperior/dscatalog/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/exceptions/ResourceExceptionHandler.java
@@ -9,21 +9,37 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.devsuperior.dscatalog.services.exceptions.DatabaseException;
 import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
+	
+	private static Integer STATUS_NOT_FOUND = HttpStatus.NOT_FOUND.value(); 
+	private static Integer STATUS_BAD_REQUEST = HttpStatus.BAD_REQUEST.value(); 
 
 	@ExceptionHandler(ResourceNotFoundException.class)
 	public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException e, HttpServletRequest request) {
 		StandardError error = new StandardError();
 		error.setTimestamp(Instant.now());
-		error.setStatus(HttpStatus.NOT_FOUND.value());
+		error.setStatus(STATUS_NOT_FOUND);
 		error.setError("Resource not found");
 		error.setMessage(e.getMessage());
 		error.setPath(request.getRequestURI());
 		
-		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+		return ResponseEntity.status(STATUS_NOT_FOUND).body(error);
+	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> databaseIntegrity(DatabaseException e, HttpServletRequest request) {
+		StandardError error = new StandardError();
+		error.setTimestamp(Instant.now());
+		error.setStatus(STATUS_BAD_REQUEST);
+		error.setError("Database integrity exception");
+		error.setMessage(e.getMessage());
+		error.setPath(request.getRequestURI());
+		
+		return ResponseEntity.status(STATUS_BAD_REQUEST).body(error);
 	}
 	
 }
