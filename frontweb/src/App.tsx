63cfbc9bed5fe661cb68ac4fe062b5996271b1c7
Rodrigo Baggio <rodrigo.baggio.si@gmail.com>
2022-01-26 11:26:41 -0300

    Navbar Styling

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,9 +1,12 @@
+import Navbar from 'components/Navbar';
+
 import './assets/styles/custom.scss';
 import './App.css';
 
 function App() {
   return (
     <div>
+      <Navbar />
       <h1>DSCatalog</h1>
     </div>
   );
