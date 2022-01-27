89ee48d4ecf12739c65c14633d611a12df01622a
Rodrigo Baggio <rodrigo.baggio.si@gmail.com>
2022-01-27 14:44:21 -0300

    ProductDetails layout mobile

--- frontweb/src/pages/ProductDetails/index.tsx
+++ frontweb/src/pages/ProductDetails/index.tsx
@@ -1,8 +1,47 @@
+import ProductPrice from 'components/ProductPrice';
+import { ReactComponent as ArrowIcon } from '../../assets/styles/images/arrow.svg';
+
+import './styles.css';
+
 const ProductDetails = () => {
   return (
-    <>
-      <h1>ProductDetails</h1>
-    </>
+    <div className="product-details-container">
+      <div className="product-details-card base-card">
+        <div className="product-details-goback-container">
+          <ArrowIcon />
+          <h2>Voltar</h2>
+        </div>
+        <div className="row">
+          <div className="col-xl-6">
+            <div className="product-details-img-container">
+              <img
+                src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg"
+                alt="produto"
+              />
+            </div>
+            <div className="product-details-name-price-container">
+              <h1>Nome do produto</h1>
+              <ProductPrice price={2190.0} />
+            </div>
+          </div>
+          <div className="col-xl-6">
+            <div className="product-details-description-container">
+              <h2>Descrição do produto</h2>
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Temporibus, officiis. Lorem ipsum dolor sit amet consectetur
+                adipisicing elit. Fuga minima placeat laboriosam architecto
+                quos, qui harum inventore blanditiis ducimus numquam cumque
+                eaque consequatur tempore repudiandae ullam eum totam ipsum
+                autem sint in. Molestiae quos voluptatibus odio expedita
+                accusantium! A eaque ducimus alias et commodi iste
+                reprehenderit, esse aut? Hic, officiis?
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
