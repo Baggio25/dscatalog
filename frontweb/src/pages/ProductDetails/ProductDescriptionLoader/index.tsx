63119e80e49254ec308205c844ba7a2c2e8c76ee
Rodrigo Baggio <rodrigo.baggio.si@gmail.com>
2022-01-28 14:09:17 -0300

    Product details loaders

---
+++ frontweb/src/pages/ProductDetails/ProductDescriptionLoader/index.tsx
@@ -0,0 +1,15 @@
+import ContentLoader from 'react-content-loader';
+
+const ProductDescriptionLoader = () => (
+  <ContentLoader
+    viewBox="0 0 320 280"
+    height={280}
+    width={320}
+    backgroundColor="#ecebeb"
+    foregroundColor="#d6d2d2"
+  >
+    <rect x="0" y="0" rx="10" ry="10" width="280" height="180" />
+  </ContentLoader>
+);
+
+export default ProductDescriptionLoader;
--- frontweb/src/pages/ProductDetails/index.tsx
+++ frontweb/src/pages/ProductDetails/index.tsx
@@ -8,6 +8,8 @@ import { Product } from 'types/product';
 import { BASE_URL } from 'util/requests';
 
 import './styles.css';
+import ProductInfoLoader from './ProductInfoLoader';
+import ProductDescriptionLoader from './ProductDescriptionLoader/index';
 
 type UrlParams = {
   productId: string;
@@ -16,11 +18,18 @@ type UrlParams = {
 const ProductDetails = () => {
   const { productId } = useParams<UrlParams>(); //captura os parametros de url
   const [product, setProduct] = useState<Product>();
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    axios.get(`${BASE_URL}/products/${productId}`).then((response) => {
-      setProduct(response.data);
-    });
+    setIsLoading(true);
+    axios
+      .get(`${BASE_URL}/products/${productId}`)
+      .then((response) => {
+        setProduct(response.data);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
   }, [productId]);
 
   return (
@@ -34,19 +43,29 @@ const ProductDetails = () => {
         </Link>
         <div className="row">
           <div className="col-xl-6">
-            <div className="product-details-img-container">
-              <img src={product?.imgUrl} alt={product?.name} />
-            </div>
-            <div className="product-details-name-price-container">
-              <h1>{product?.name}</h1>
-              {product && <ProductPrice price={product?.price} />}
-            </div>
+            {isLoading ? (
+              <ProductInfoLoader />
+            ) : (
+              <>
+                <div className="product-details-img-container">
+                  <img src={product?.imgUrl} alt={product?.name} />
+                </div>
+                <div className="product-details-name-price-container">
+                  <h1>{product?.name}</h1>
+                  {product && <ProductPrice price={product?.price} />}
+                </div>
+              </>
+            )}
           </div>
           <div className="col-xl-6">
-            <div className="product-details-description-container">
-              <h2>Descrição do produto</h2>
-              <p>{product?.description}</p>
-            </div>
+            {isLoading ? (
+              <ProductDescriptionLoader />
+            ) : (
+              <div className="product-details-description-container">
+                <h2>Descrição do produto</h2>
+                <p>{product?.description}</p>
+              </div>
+            )}
           </div>
         </div>
       </div>
