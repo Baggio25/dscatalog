00673a317377b543b50305b50f42f51cc735b445
Rodrigo Baggio <rodrigo.baggio.si@gmail.com>
2022-02-01 16:04:33 -0300

    Fix Repository

--- frontweb/src/util/formatters.ts
+++
@@ -1,4 +0,0 @@
-export const formatPrice = (price: number) => {
-  const params = { maximumFractionDigits: 2, minimumFractionDigits: 2 };
-  return Intl.NumberFormat('pt-BR', params).format(price);
-} 
