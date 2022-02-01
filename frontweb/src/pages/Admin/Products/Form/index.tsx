ba8861258321640388802a5ca1d288627083a094
Rodrigo Baggio <rodrigo.baggio.si@gmail.com>
2022-02-01 17:06:32 -0300

    Product Routes

---
+++ frontweb/src/pages/Admin/Products/Form/index.tsx
@@ -0,0 +1,9 @@
+const Form = () => {
+  return (
+    <div>
+      <h1>Formulário de produtos</h1>
+    </div>
+  );
+};
+
+export default Form;
---
+++ frontweb/src/pages/Admin/Products/List/index.tsx
@@ -0,0 +1,9 @@
+const List = () => {
+  return (
+    <div>
+      <h1>Listagem de produtos</h1>
+    </div>
+  );
+};
+
+export default List;
