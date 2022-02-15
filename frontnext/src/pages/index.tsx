4e6094f87d006be08c87fd43fc8e5166510e6d12
Rodrigo Baggio <rodrigo.baggio.si@gmail.com>
2022-02-15 16:20:33 -0300

    Homepage Part 1

--- frontnext/src/pages/index.tsx
+++ frontnext/src/pages/index.tsx
@@ -1,11 +1,26 @@
-import type { NextPage } from "next";
+import { ButtonIcon } from "../components";
+import styles from "../styles/pages/home.module.css";
 
-const Home: NextPage = () => {
+export default function Home() {
   return (
-    <div>
-      <h3>DSCatalog</h3>
+    <div className={styles.homeContainer}>
+      <div
+        className={`row d-flex flex-column-reverse align-items-center justify-content-center 
+          flex-lg-row card-base border-radius-20 ${styles.homeText}`}
+      >
+        <div
+          className={`column-lg-6 d-flex flex-column align-items-center mt-4 mt-md-0 ${styles.homeText}`}
+        >
+          <h1 className={styles.textTitle}>
+            Conheça o melhor catálogo de produtos
+          </h1>
+          <p className={styles.textSubTitle}>
+            Ajudaremos você a encontrar os melhores produtos disponíveis no
+            mercado.
+          </p>
+          <ButtonIcon label="Inicie agora a sua busca" />
+        </div>
+      </div>
     </div>
   );
-};
-
-export default Home;
+}
