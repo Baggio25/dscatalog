bbdbeedd697532a63e6a6961e7ca293d00d81ce7
rodrigo.baggio.si@gmail.com <Baggio25>
2022-04-29 16:46:06 -0300

    Catalog

---
+++ frontnext/src/components/ProductItem/index.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import productImage from '../../../public/product.png';
+import ProductPrice from "../ProductPrice";
+import styles from "./productitem.module.css";
+
+export default function ProductItem() {
+  return (
+    <Link href="/catalog/product/1">
+      <a className={`card-base border-radius-10 ${styles.productCard}`}>
+          <div className={styles.cardTopContainer}>
+              <Image src={productImage} alt="Nome do produto" className={styles.productCardImage}/>
+          </div>
+          <div className={styles.cardBottomContainer}>
+              <h6>Nome do produto</h6>
+              <ProductPrice price="1999,90" />
+          </div>
+      </a>
+    </Link>
+  );
+}
