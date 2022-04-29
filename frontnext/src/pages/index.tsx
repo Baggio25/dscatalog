f518883e30ff4ee2c8821c84650394bd95038a5a
rodrigo.baggio.si@gmail.com <Baggio25>
2022-04-29 15:51:36 -0300

    HomePage part 2

--- frontnext/src/pages/index.tsx
+++ frontnext/src/pages/index.tsx
@@ -1,5 +1,11 @@
+/* eslint-disable @next/next/link-passhref */
 import type { NextPage } from "next";
+import Image from "next/image";
+import Link from "next/link";
 
+import { ButtonIcon } from "../components";
+
+import homeImage from "../../public/homeimage.svg";
 import styles from "../styles/pages/home.module.css";
 
 const Home: NextPage = () => {
@@ -7,18 +13,34 @@ const Home: NextPage = () => {
     <div className={styles.homeContainer}>
       <div
         className={`row d-flex flex-column-reverse align-items-center 
-        justify-content-center flex-lg-row card-base border-radius-20 ${styles.homeText}`}
+        justify-content-center flex-lg-row ${styles.homeContent}`}
       >
         <div
-          className={`col-lg-6 d-flex flex-colum align-items-center mt-4 mt-md-0 ${styles.homeText}`}
+          className={`col-lg-6 d-flex flex-column align-items-center 
+          mt-4 mt-md-0 ${styles.homeText}`}
         >
           <h1 className={styles.textTitle}>
             Conheça o melhor catálogo de produtos
           </h1>
+
           <p className={styles.textSubtitle}>
             Ajudaremos você a encontrar os melhores produtos disponíveis no
             mercado.
           </p>
+
+          <Link href="/catalog">
+            <a>
+              <ButtonIcon label="Inicie agora a sua busca" />
+            </a>
+          </Link>
+        </div>
+
+        <div
+          className={`col-lg-6 d-flex align-items-center 
+          justify-content-center align-items-lg-center 
+          justify-content-lg-start ${styles.homeImgContainer}`}
+        >
+          <Image src={homeImage} alt="HomePage Image" />
         </div>
       </div>
     </div>
