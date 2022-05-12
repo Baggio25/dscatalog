a9d50eae3e7285773585846eb463d549d8aa88e4
rodrigo.baggio.si@gmail.com <Baggio25>
2022-05-12 14:04:10 -0300

    Listing products p2

--- frontnext/src/components/ProductRow/index.tsx
+++ frontnext/src/components/ProductRow/index.tsx
@@ -1,11 +1,37 @@
 import { ProductItemProps } from "../../@types";
+import ProductPrice from "../ProductPrice";
 
 import styles from "./productrow.module.css";
 
 export default function ProductRow(product: ProductItemProps) {
   return (
-    <div>
-      <p>{product.name}</p>
+    <div
+      className={`d-flex flex-column flex-md-row align-items-center p-4 ${styles.productRowContainer}`}
+    >
+      <div className={styles.imgContainer}>
+        <img src={product.imgUrl} alt={product.name} />
+      </div>
+      <div className={styles.productRowDetails}>
+        <h4>{product.name}</h4>
+        <ProductPrice price={String(product.price)} />
+        <div className={`d-flex flex-row ${styles.categories}`}>
+          {product.categories.map((category) => (
+            <div className={styles.category} key={category.id}>
+              {category.name}
+            </div>
+          ))}
+        </div>
+      </div>
+      <div
+        className={`d-flex flex-row-reverse flex-md-column justify-content-around ${styles.buttonContainer}`}
+      >
+        <button type="button" className={styles.buttonEdit}>
+          Editar
+        </button>
+        <button type="button" className={styles.buttonDelete}>
+          Excluir
+        </button>
+      </div>
     </div>
   );
 }
--- frontnext/src/pages/admin/dashboard/pages/products/index.tsx
+++ frontnext/src/pages/admin/dashboard/pages/products/index.tsx
@@ -3,12 +3,10 @@ import { ProductRow } from "../../../../../components";
 
 function Products({ products }: ProductsResponse) {
   return (
-    <div>
-      <div style={{width: "100%", padding: 40}}>
-        {products.map((product) => (
-          <ProductRow {...product} key={product.id} />
-        ))}
-      </div>
+    <div style={{ width: "100%", padding: 40 }}>
+      {products.map((product) => (
+        <ProductRow {...product} key={product.id} />
+      ))}
     </div>
   );
 }
